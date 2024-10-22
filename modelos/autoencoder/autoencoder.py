0609ee1a3fe0067e159cbd7e351d8d604b22cfbd
manoloFer10 <112580262+manoloFer10@users.noreply.github.com>
2024-10-21 22:42:53 -0300

    Pruebas con el autoencoder y un mlp

--- modelos/autoencoder/autoencoder.py
+++ modelos/autoencoder/autoencoder.py
@@ -2,10 +2,15 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Autoencoder(nn.Module):
-    def __init__(self, dim_activaciones: int, dim_rala: int):
+    '''
+        Vi que la cabra Jake suma una funcion para normalizar los pesos del decoder. No termino de entender por qué lo hace así
+        que no lo sumé.
+    '''
+    def __init__(self, dim_activaciones: int, dim_rala: int, dataset_geometric_median):
         super().__init__()
 
         self.encoder = nn.Sequential(
@@ -19,7 +24,8 @@ class Autoencoder(nn.Module):
 
         # Este es el pre encoder bias que muestran en las cuentas del autoencoder antes de entrar en el encoder.
         # Entiendo que es un bias distinto al de la capa encoder porque ese va después de aplicar la matriz de pesos.
-        self.pre_encoder_bias = nn.Parameter(torch.Tensor())
+        # Inicializamos como la mediana geométrica del dataset (lo dice en el paper de anthropic).
+        self.pre_encoder_bias = nn.Parameter(torch.Tensor(dataset_geometric_median))
 
     def encode(self, x):
         x = x - self.pre_encoder_bias
@@ -28,38 +34,59 @@ class Autoencoder(nn.Module):
     def decode(self, x):
         return self.decoder(x) + self.pre_encoder_bias
 
+    def forward(self, x):
+
+        encoded = self.encode(x)
+        decoded = self.decode(encoded)
+
+        return encoded, decoded
+
+    def normalize_decoder_weights(self):
+        with torch.no_grad():
+            self.decoder.weight.data = nn.functional.normalize(self.decoder.weight.data, p=2, dim=1)
+
 
 class lossAutoencoder(nn.Module):
-    def __init__(self, autoencoder: Autoencoder, lasso_lambda: int = 1e-3):
+    def __init__(self, lasso_lambda: int = 1e-3):
         super(lossAutoencoder, self).__init__()
-        self.autoencoder = autoencoder
         self.lasso_lambda = lasso_lambda
 
     def lasso_loss(self, f):
         l1 = torch.norm(f, p=1)
         return self.lasso_lambda * l1
 
-    def forward(self, input_activaciones):
+    def forward(self, input_activaciones, encoded, decoded):
         mse = nn.MSELoss()
 
-        f = self.autoencoder.encode(input_activaciones)
-        reconstruccion = self.autoencoder.decode(f)
-
-        ecm = mse(reconstruccion, input_activaciones)
-        lasso = self.autoencoder.lasso_loss(f)
+        ecm = mse(decoded, input_activaciones)
+        lasso = self.lasso_loss(encoded)
 
         return ecm + lasso
 
-'''
-PENDIENTE
-'''
-def entrenar_autoencoder(autoencoder: Autoencoder, activaciones:Dataset, lasso_lambda: int, epochs: int, batch_size: int, learning_rate=1e-3):
+
+#TODO: terminar el .train() del autoencoder para usar en el bucle de entrenamiento con el transformer.
+
+
+# Lo de abajo lo dejo para reciclar cuando tengamos que entrenar. Me fui al pasto.
+
+def entrenar_autoencoder(autoencoder: Autoencoder, activaciones: Dataset, lasso_lambda: int, epochs: int,
+                         batch_size: int, learning_rate=1e-3):
     # Preparamos el dataset:
     # Hacemos un dataset customizado de activaciones de neuronas suponiendo que activaciones
     # es un numpy array.
+
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
     class DatasetActivaciones(Dataset):
         def __init__(self, activaciones):
-            self.activaciones = [torch.tensor(lista_activaciones, dtype=torch.float32) for lista_activaciones in activaciones]
+            self.activaciones = [torch.tensor(lista_activaciones, dtype=torch.float32) for lista_activaciones in
+                                 activaciones]
 
         def __len__(self):
             return len(self.activaciones)
@@ -75,27 +102,29 @@ def entrenar_autoencoder(autoencoder: Autoencoder, activaciones:Dataset, lasso_l
 
     loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
-
-    #Definimos el optimizador
+    # Definimos el optimizador
     optimizer = torch.optim.Adam(autoencoder.parameters(), lr=learning_rate)
 
-    #Definimos la función de pérdida
-    loss_function = lossAutoencoder(autoencoder, lasso_lambda)
+    # Definimos la función de pérdida
+    loss_function = lossAutoencoder(lasso_lambda)
 
-    #Entrenamiento
+    # Lista en la que iremos guardando el valor de la función de pérdida en cada
+    # etapa de entrenamiento
+    loss_list = []
+
+    # Entrenamiento
     for epoch in range(epochs):
 
-        total_loss= .0
+        total_loss = .0
 
         for x_train in loader:
+            input_data = x_train.to(device)
             optimizer.zero_grad()
 
-            '''
-            ¿Cómo hago para computar el forward, la loss y después el backward? Si computo la loss,
-            ¿cuenta como haber pasado forward?
-            '''
             # Realizo la pasada forward por la red
-            loss = loss_function(autoencoder)
+            encoded, decoded = autoencoder(input_data)
+
+            loss = loss_function(input_activaciones = x_train, encoded = encoded, decoded =decoded)
 
             # Realizo la pasada backward por la red
             loss.backward()
@@ -107,14 +136,14 @@ def entrenar_autoencoder(autoencoder: Autoencoder, activaciones:Dataset, lasso_l
             loss_list.append(loss.data.item())
 
             # Acumulo la loss del minibatch
-            total_loss += loss.item() * y.size(0)
+            total_loss += loss.item() * x_train.size(0)
 
             # Normalizo la loss total
         total_loss /= len(loader.dataset)
 
         # Muestro el valor de la función de pérdida cada 100 iteraciones
-        if i > 0 and i % 100 == 0:
-            print('Epoch %d, loss = %g' % (i, total_loss))
+        if epoch > 0 and epoch % 100 == 0:
+            print('Epoch %d, loss = %g' % (epoch, total_loss))
 
     # Muestro la lista que contiene los valores de la función de pérdida
     # y una versión suavizada (rojo) para observar la tendencia
