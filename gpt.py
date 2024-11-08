e4a336e5763fabac7e03c9d4400c6fd3c8712de9
Joaquin Polonuer <jtpolonuer@gmail.com>
2024-11-08 18:29:26 -0300

    added greedy generate to gpt

--- gpt.py
+++ gpt.py
@@ -208,6 +208,23 @@ class GPTLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
         return idx
 
+    def greedy_generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last context_length tokens
+            idx_cond = idx[:, -self.context_length :]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next = torch.argmax(probs, dim=-1).unsqueeze(1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
     def embed(self, idx):
         _, T = idx.shape
         tok_emb = self.token_embedding_table(idx)
