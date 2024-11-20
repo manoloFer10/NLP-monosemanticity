5c43b1ae934e23b3fafe366cece98f05647a6041
Nicopiwi <nikorozenberg@gmail.com>
2024-11-20 20:51:09 -0300

    changed mlflow env

--- mlflow_env.py
+++ mlflow_env.py
@@ -3,4 +3,4 @@ import mlflow
 import sys
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
-mlflow.set_tracking_uri("http://127.0.0.1:5001")
+mlflow.set_tracking_uri("http://34.176.189.11:5000/")
